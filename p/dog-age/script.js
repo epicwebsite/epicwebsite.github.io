2a414830263e6bd22934c14425f3cb41656ec9e2
DarccMan <darcy3057@gmail.com>
2021-01-01 18:50:13 +1100

    Fixed dog-age site, 404 page

--- p/dog-age/script.js
+++ p/dog-age/script.js
@@ -3,13 +3,13 @@ var av = {
   days: 30.46,
   days2: 365,
 }
+var date = "2019-02-15";
 function init() {
   mod_change();
   input_change();
-  var date = "2019-02-15";
-  f.docId("i_years").value = getDateDiff(date).years;
-  f.docId("i_months").value = getDateDiff(date).months;
-  f.docId("i_days").value = getDateDiff(date).days;
+  doc.id("i_years").value = getDateDiff(date).years;
+  doc.id("i_months").value = getDateDiff(date).months;
+  doc.id("i_days").value = getDateDiff(date).days;
   calc();
 }
 function getDateDiff(date) {
@@ -32,72 +32,96 @@ function getDateDiff(date) {
   });
 }
 function calc() {
-  var date = "2019-02-15";
-  if ([null, undefined, ""].includes(f.docId("i_years").value)) {
-    f.docId("i_years").value = getDateDiff(date).years;
+  if ([null, undefined, ""].includes(doc.id("i_years").value)) {
+    doc.id("i_years").value = getDateDiff(date).years;
   }
-  if ([null, undefined, ""].includes(f.docId("i_months").value)) {
-    f.docId("i_months").value = getDateDiff(date).months;
+  if ([null, undefined, ""].includes(doc.id("i_months").value)) {
+    doc.id("i_months").value = getDateDiff(date).months;
   }
-  if ([null, undefined, ""].includes(f.docId("i_days").value)) {
-    f.docId("i_days").value = getDateDiff(date).days;
+  if ([null, undefined, ""].includes(doc.id("i_days").value)) {
+    doc.id("i_days").value = getDateDiff(date).days;
   }
-  var years = parseInt(f.docId("i_years").value);
-  var months = parseInt(f.docId("i_months").value);
-  var days = parseInt(f.docId("i_days").value);
-  if (f.docId("input_type").value == "bday") {
-    var date = f.docId("i_date").value;
+
+  var years = parseInt(doc.id("i_years").value);
+  var months = parseInt(doc.id("i_months").value);
+  var days = parseInt(doc.id("i_days").value);
+  if (doc.id("input_type").value == "bday") {
+    var date = doc.id("i_date").value;
     years = getDateDiff(date).years;
     months = getDateDiff(date).months;
     days = getDateDiff(date).days;
   }
-  if (f.docId("mod").value == "other") {
-    var mod = parseInt(f.docId("i_mod").value);
+  if (doc.id("mod").value.split("_").sub(0) == "other") {
+    var mod = parseInt(doc.id("i_mod").value);
   } else {
-    var mod = parseInt(f.docId("mod").value);
+    var mod = parseInt(doc.id("mod").value.split("_").sub(0));
   }
-  // Convert months to days
+
   days += (months * av.days);
   months = 0;
-  // Convert years to months
   months = (years * av.months);
   years = 0;
-  // Convert months to days
   days += (months * av.days);
   months = 0;
-  // Times days by amount
   days *= mod;
-  // Convert days to years
   years = Math.floor(days / av.days2);
-  // Remove days that were converted into years
   days = days - (av.days2 * years);
-  // Convert days to months
   months = Math.floor(days / av.days);
   days = days - (av.days * months);
-  // Round days
   days = Math.round(days);
-  // Output
-  console.log("{0} years, {1} months, {2} days".format(years, months, days));
-  f.docId("o_years").value = years;
-  f.docId("o_months").value = months;
-  f.docId("o_days").value = days;
-  if (months == 0 && days == 0) {
-    f.docId("bday").innerHTML = "Happy Birthday!";
+
+  output = "Your {0} is {1} year{2}, {3} month{4}, {5} day{6}".format(
+    doc.id("mod").value.split("_")[1],
+    years,
+    (years == 1) ? "" : "s",
+    months,
+    (months == 1) ? "" : "s",
+    days,
+    (days == 1) ? "" : "s",
+  );
+  doc.id("output").innerHTML = output;
+
+  d = new Date();
+  if (
+    parseInt(doc.id("i_date").value.split("-")[2]) == d.getDate()
+    && parseInt(doc.id("i_date").value.split("-")[1]) == (d.getMonth() + 1)
+  ) {
+    doc.id("bday_h").style.display = "block";
+  } else {
+    doc.id("bday_h").style.display = "none";
+  }
+  if (
+    months == 0
+    && days == 0
+  ) {
+    doc.id("bday_d").style.display = "block";
+  } else {
+    doc.id("bday_d").style.display = "none";
   }
 }
 function mod_change() {
-  if (f.docId("mod").value == "other") {
-    f.docId("i_mod").style.display = "block";
+  if (doc.id("mod").value == "other") {
+    doc.id("i_mod").style.display = "block";
   } else {
-    f.docId("i_mod").style.display = "none";
+    doc.id("i_mod").style.display = "none";
   }
+  doc.id("animal").innerHTML = doc.id("mod").value.split("_").sub(-1);
 }
 function input_change() {
-  if (f.docId("input_type").value == "bday") {
-    f.docId("input_bday").style.display = "block";
-    f.docId("input_age").style.display = "none";
+  if (doc.id("input_type").value == "bday") {
+    doc.id("input_bday").style.display = "block";
+    // doc.id("input_age").style.display = "none";
   } else {
-    f.docId("input_age").style.display = "block";
-    f.docId("input_bday").style.display = "none";
+    doc.id("input_age").style.display = "block";
+    // doc.id("input_bday").style.display = "none";
   }
+}
+
+function bday_change() {
+  doc.id("i_years").value = getDateDiff(doc.id("i_date").value).years;
+  doc.id("i_months").value = getDateDiff(doc.id("i_date").value).months;
+  doc.id("i_days").value = getDateDiff(doc.id("i_date").value).days;
+}
+function age_change() {
+  
 }
