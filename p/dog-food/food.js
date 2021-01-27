efdeffe8fc4b31e5089f03cb39bdd19165f4adf9
DarccMan <darcy3057@gmail.com>
2021-01-27 19:07:11 +1100

    Many changes to dog-food page

--- p/dog-food/food.js
+++ p/dog-food/food.js
@@ -1,96 +1,714 @@
 var food = [
   {
-    "name": "Can eat",
-    "items": [
-      "Apple",
-      "Banana",
-      "Beans",
-      "Blackberries",
-      "Blueberries",
-      "Bread",
-      "Broccoli",
-      "Carrots (Small pieces)",
-      "Celery (leaves with caution)",
-      "Chicken (cooked, without bones)",
-      "Chickpea",
-      "Coconut",
-      "Cooked prawns",
-      "Cooked rice",
-      "Corn (not cob)",
-      "Cucumbers",
-      "Eggs (cooked)",
-      "Fish (skin is good)",
-      "Ginger",
-      "Mango",
-      "Meat (Pork, Beef, Turkey, ect)",
-      "Oats / Oatmeal",
-      "Pasta",
-      "Peanut butter",
-      "Peas",
-      "Popcorn",
-      "Potato",
-      "Pumpkin (flesh, seeds)",
-      "Quinoa",
-      "Strawberries",
-      "Sweet Potato",
-      "Watermelon",
-      "Weetbix",
-      "Yogurt"
-    ]
+    name: "Can eat",
+    items: [
+      {
+        name: "Apple",
+        subtitle: "Not core",
+        tags: [
+          "apple",
+          "fruit",
+          "core",
+        ],
+        desc: "",
+      },
+      {
+        name: "Banana",
+        subtitle: "",
+        tags: [
+          "peel",
+          "fruit",
+        ],
+        desc: "",
+      },
+      {
+        name: "Beans",
+        subtitle: "",
+        tags: [
+          "bean",
+          "lentil",
+          "baked",
+        ],
+        desc: "",
+      },
+      {
+        name: "Blackberries",
+        subtitle: "",
+        tags: [
+          "berry",
+          "berrie",
+          "black",
+          "fruit",
+        ],
+        desc: "",
+      },
+      {
+        name: "Blueberries",
+        subtitle: "",
+        tags: [
+          "berry",
+          "berrie",
+          "black",
+          "fruit",
+        ],
+        desc: "",
+      },
+      {
+        name: "Bread",
+        subtitle: "",
+        tags: [
+          "dough",
+          "gluten",
+          "loaf",
+        ],
+        desc: "",
+      },
+      {
+        name: "Broccoli",
+        subtitle: "",
+        tags: [
+          "brocoli",
+          "brocolli",
+          "broccolli",
+          "vege",
+        ],
+        desc: "",
+      },
+      {
+        name: "Carrots",
+        subtitle: "Small pieces",
+        tags: [
+          "root",
+          "vege",
+        ],
+        desc: "",
+      },
+      {
+        name: "Celery",
+        subtitle: "Leaves with caution",
+        tags: [
+          "vege",
+        ],
+        desc: "",
+      },
+      {
+        name: "Chicken",
+        subtitle: "Cooked, without bones",
+        tags: [
+          "poultry",
+          "meat",
+        ],
+        desc: "",
+      },
+      {
+        name: "Chickpea",
+        subtitle: "",
+        tags: [
+          "",
+        ],
+        desc: "",
+      },
+      {
+        name: "Coconut",
+        subtitle: "",
+        tags: [
+          "fruit",
+        ],
+        desc: "",
+      },
+      {
+        name: "Cooked prawns",
+        subtitle: "",
+        tags: [
+          "shrimp",
+          "prawn",
+          "seafood",
+        ],
+        desc: "",
+      },
+      {
+        name: "Cooked rice",
+        subtitle: "",
+        tags: [
+          "",
+        ],
+        desc: "",
+      },
+      {
+        name: "Corn",
+        subtitle: "Not the cob",
+        tags: [
+          "vege",
+        ],
+        desc: "",
+      },
+      {
+        name: "Cucumbers",
+        subtitle: "",
+        tags: [
+          "vege",
+        ],
+        desc: "",
+      },
+      {
+        name: "Eggs",
+        subtitle: "Cooked",
+        tags: [
+          "chicken",
+          "poultry",
+        ],
+        desc: "",
+      },
+      {
+        name: "Fish",
+        subtitle: "Skin is good for coar",
+        tags: [
+          "tuna",
+          "salmon",
+          "seafood",
+        ],
+        desc: "",
+      },
+      {
+        name: "Ginger",
+        subtitle: "",
+        tags: [
+          "root",
+          "vege",
+        ],
+        desc: "",
+      },
+      {
+        name: "Mango",
+        subtitle: "",
+        tags: [
+          "fruit",
+        ],
+        desc: "",
+      },
+      {
+        name: "Meat",
+        subtitle: "Pork, Beef, Turkey, ect",
+        tags: [
+          "pork",
+          "beef",
+          "turkey",
+          "chicken",
+          "lamb",
+        ],
+        desc: "",
+      },
+      {
+        name: "Oats / Oatmeal",
+        subtitle: "",
+        tags: [
+          "oat",
+        ],
+        desc: "",
+      },
+      {
+        name: "Pasta",
+        subtitle: "",
+        tags: [
+          "spaghetti",
+          "noodles",
+        ],
+        desc: "",
+      },
+      {
+        name: "Peanut butter",
+        subtitle: "",
+        tags: [
+          "nut",
+        ],
+        desc: "",
+      },
+      {
+        name: "Peas",
+        subtitle: "",
+        tags: [
+          "pea",
+          "bean",
+          "vege",
+        ],
+        desc: "",
+      },
+      {
+        name: "Popcorn",
+        subtitle: "",
+        tags: [
+          "corn",
+          "vege",
+        ],
+        desc: "",
+      },
+      {
+        name: "Potato",
+        subtitle: "",
+        tags: [
+          "root",
+          "vege",
+        ],
+        desc: "",
+      },
+      {
+        name: "Pumpkin",
+        subtitle: "Flesh or seeds",
+        tags: [
+          "vege",
+        ],
+        desc: "",
+      },
+      {
+        name: "Quinoa",
+        subtitle: "",
+        tags: [
+          "",
+        ],
+        desc: "",
+      },
+      {
+        name: "Strawberries",
+        subtitle: "",
+        tags: [
+          "strawberry",
+          "berry",
+          "berries",
+          "fruit",
+        ],
+        desc: "",
+      },
+      {
+        name: "Sweet Potato",
+        subtitle: "",
+        tags: [
+          "vege",
+          "root",
+        ],
+        desc: "",
+      },
+      {
+        name: "Watermelon",
+        subtitle: "",
+        tags: [
+          "melon",
+          "fruit",
+        ],
+        desc: "",
+      },
+      {
+        name: "Weetbix",
+        subtitle: "",
+        tags: [
+          "wheat",
+          "cereal",
+          "wheaties",
+        ],
+        desc: "",
+      },
+      {
+        name: "Yogurt",
+        subtitle: "",
+        tags: [
+          "",
+        ],
+        desc: "",
+      },
+    ],
   },
   {
-    "name": "Limit / Caution",
-    "items": [
-      "Almonds",
-      "Capsicum",
-      "Cashews",
-      "Cheese",
-      "Cherries (flesh only)",
-      "Honey",
-      "Milk",
-      "Peanuts",
-      "Raw meat (careful)",
-      "Salt",
-      "Salty meats (ham, salami)",
-      "Sausage ",
-      "Tomato",
-    ]
+    name: "Limit / Caution",
+    items: [
+      {
+        name: "Almonds",
+        subtitle: "",
+        tags: [
+          "nut",
+        ],
+        desc: "",
+      },
+      {
+        name: "Capsicum",
+        subtitle: "",
+        tags: [
+          "pepper",
+          "vege",
+        ],
+        desc: "",
+      },
+      {
+        name: "Cashews",
+        subtitle: "",
+        tags: [
+          "nut",
+        ],
+        desc: "",
+      },
+      {
+        name: "Cheese",
+        subtitle: "",
+        tags: [
+          "dairy",
+          "chedder",
+          "milk",
+        ],
+        desc: "",
+      },
+      {
+        name: "Cherries",
+        subtitle: "Flesh only",
+        tags: [
+          "berry",
+          "berries",
+          "fruit",
+        ],
+        desc: "",
+      },
+      {
+        name: "Honey",
+        subtitle: "",
+        tags: [
+          "bee",
+        ],
+        desc: "",
+      },
+      {
+        name: "Milk",
+        subtitle: "",
+        tags: [
+          "dairy",
+        ],
+        desc: "",
+      },
+      {
+        name: "Peanuts",
+        subtitle: "",
+        tags: [
+          "nut",
+        ],
+        desc: "",
+      },
+      {
+        name: "Raw meat",
+        subtitle: "",
+        tags: [
+          "",
+        ],
+        desc: "",
+      },
+      {
+        name: "Salt",
+        subtitle: "",
+        tags: [
+          "",
+        ],
+        desc: "",
+      },
+      {
+        name: "Salty meats",
+        subtitle: "Ham, Salami",
+        tags: [
+          "",
+        ],
+        desc: "",
+      },
+      {
+        name: "Sausage",
+        subtitle: "",
+        tags: [
+          "meat",
+        ],
+        desc: "",
+      },
+      {
+        name: "Tomato",
+        subtitle: "",
+        tags: [
+          "fruit",
+          "vege",
+        ],
+        desc: "",
+      },
+    ],
   },
   {
-    "name": "CANNOT EAT!",
-    "items": [
-      "Alcohol & meds (obviously)",
-      "Avocado",
-      "Cherry pips, stems, leaves (flesh is ok)",
-      "Chewing gum",
-      "Chicken bones / cooked bones",
-      "Chocolate",
-      "Cinnamon",
-      "Citrus, orange, lemon, lime",
-      "Coconut products (water, milk, ect)",
-      "Coffee / tea",
-      "Corn cobs",
-      "Falafel ",
-      "Fatty meats (offcuts)",
-      "Grapes, raisins, currants",
-      "Hommus",
-      "Ice cream",
-      "Macadamia nuts",
-      "Mouldy food / compost",
-      "Mushrooms",
-      "Nutmeg",
-      "Onion, garlic, chives",
-      "Peach, plum, persimmon",
-      "Pineapple",
-      "Pistachio nuts",
-      "Pumpkin skin, stem",
-      "Raw egg",
-      "Raw yeast dough",
-      "Soft drink / ginger beer",
-      "Tea tree oil ",
-      "Uncooked prawns",
-      "Vegemite",
-      "Xylitol (sugar substitute)"
-    ]
-  }
+    name: "CANNOT EAT!",
+    items: [
+      {
+        name: "Alcohol & medication",
+        subtitle: "Obviously.",
+        tags: [
+          "meds",
+        ],
+        desc: "",
+      },
+      {
+        name: "Avocado",
+        subtitle: "",
+        tags: [
+          "vege",
+        ],
+        desc: "",
+      },
+      {
+        name: "Cherry pips, stems, leaves",
+        subtitle: "Flesh is ok",
+        tags: [
+          "berry",
+          "berries",
+          "fruit",
+        ],
+        desc: "",
+      },
+      {
+        name: "Chewing gum",
+        subtitle: "",
+        tags: [
+          "gum",
+          "bubble",
+        ],
+        desc: "",
+      },
+      {
+        name: "Chicken bones / cooked bones",
+        subtitle: "Can splinter",
+        tags: [
+          "poulty",
+          "meat",
+        ],
+        desc: "",
+      },
+      {
+        name: "Chocolate",
+        subtitle: "",
+        tags: [
+          "",
+        ],
+        desc: "",
+      },
+      {
+        name: "Cinnamon",
+        subtitle: "",
+        tags: [
+          "spice",
+        ],
+        desc: "",
+      },
+      {
+        name: "Citrus fruit",
+        subtitle: "",
+        tags: [
+          "orange",
+          "lemon",
+          "lime",
+          "fruit",
+        ],
+        desc: "",
+      },
+      {
+        name: "Coconut products",
+        subtitle: "Water, Milk",
+        tags: [
+          "fruit",
+        ],
+        desc: "",
+      },
+      {
+        name: "Coffee / tea",
+        subtitle: "",
+        tags: [
+          "caffeine",
+        ],
+        desc: "",
+      },
+      {
+        name: "Corn cobs",
+        subtitle: "",
+        tags: [
+          "vege",
+        ],
+        desc: "",
+      },
+      {
+        name: "Falafel",
+        subtitle: "",
+        tags: [
+          "chickpea",
+        ],
+        desc: "",
+      },
+      {
+        name: "Fatty meats",
+        subtitle: "Offcuts",
+        tags: [
+          "",
+        ],
+        desc: "",
+      },
+      {
+        name: "Grapes, raisins, currants",
+        subtitle: "",
+        tags: [
+          "berry",
+          "berries",
+          "fruit",
+        ],
+        desc: "",
+      },
+      {
+        name: "Hommus",
+        subtitle: "",
+        tags: [
+          "chickpea",
+        ],
+        desc: "",
+      },
+      {
+        name: "Ice cream",
+        subtitle: "",
+        tags: [
+          "dairy",
+          "dessert",
+        ],
+        desc: "",
+      },
+      {
+        name: "Macadamia nuts",
+        subtitle: "",
+        tags: [
+          "nut",
+        ],
+        desc: "",
+      },
+      {
+        name: "Mouldy food / compost",
+        subtitle: "",
+        tags: [
+          "off",
+          "spoil",
+        ],
+        desc: "",
+      },
+      {
+        name: "Mushrooms",
+        subtitle: "",
+        tags: [
+          "fungi",
+          "fungus",
+          "vege",
+        ],
+        desc: "",
+      },
+      {
+        name: "Nutmeg",
+        subtitle: "",
+        tags: [
+          "spice",
+        ],
+        desc: "",
+      },
+      {
+        name: "Onion, garlic, chives",
+        subtitle: "",
+        tags: [
+          "shallot",
+          "vege",
+        ],
+        desc: "",
+      },
+      {
+        name: "Peach, plum, persimmon",
+        subtitle: "",
+        tags: [
+          "fruit",
+        ],
+        desc: "",
+      },
+      {
+        name: "Pineapple",
+        subtitle: "",
+        tags: [
+          "fruit",
+        ],
+        desc: "",
+      },
+      {
+        name: "Pistachio nuts",
+        subtitle: "",
+        tags: [
+          "nut",
+        ],
+        desc: "",
+      },
+      {
+        name: "Pumpkin skin, stem",
+        subtitle: "",
+        tags: [
+          "vege",
+        ],
+        desc: "",
+      },
+      {
+        name: "Raw egg",
+        subtitle: "",
+        tags: [
+          "poultry",
+          "chicken",
+        ],
+        desc: "",
+      },
+      {
+        name: "Raw yeast dough",
+        subtitle: "",
+        tags: [
+          "bread",
+        ],
+        desc: "",
+      },
+      {
+        name: "Soft drink",
+        subtitle: "",
+        tags: [
+          "soda",
+          "pop",
+        ],
+        desc: "",
+      },
+      {
+        name: "Tea tree oil",
+        subtitle: "",
+        tags: [
+          "",
+        ],
+        desc: "",
+      },
+      {
+        name: "Uncooked prawns",
+        subtitle: "",
+        tags: [
+          "prawn",
+          "shrimp",
+          "seafood",
+        ],
+        desc: "",
+      },
+      {
+        name: "Vegemite",
+        subtitle: "",
+        tags: [
+          "promite",
+          "marmite",
+        ],
+        desc: "",
+      },
+      {
+        name: "Xylitol",
+        subtitle: "Sugar Substitute",
+        tags: [
+          "",
+        ],
+        desc: "",
+      },
+    ],
+  },
 ];
--- p/dog-food/script.js
+++ p/dog-food/script.js
@@ -21,9 +21,24 @@ function setTable() {
   for (i = 0; i < length; i++) {
     el = '<tr>';
     for (v = 0; v < food.length; v++) {
-      el += '<td>{0}</td>'.format(
-        food[v].items[i] ? food[v].items[i] : "",
-      );
+      el += [
+        '<td title="{title}" id="{id}">',
+        '  <button {onclick} class="tableButton {hover}">',
+        '    {name}',
+        '    <br>',
+        '    <span class="subtitle">',
+        '      {subtitle}',
+        '    </span>',
+        '  </button>',
+        '</td>'
+      ].join("").format({
+        id: "t_" + v + "_" + i,
+        onclick: food[v].items[i] ? 'onclick="clickItem(this);"' : "",
+        hover: food[v].items[i] ? "hover" : "",
+        title: food[v].items[i] && food[v].items[i].tags && food[v].items[i].tags.length > 0 && food[v].items[i].tags[0] ? "Search Terms: " + food[v].items[i].tags.join(", ").capWords() : "",
+        subtitle: food[v].items[i] && food[v].items[i].subtitle ? food[v].items[i].subtitle : "",
+        name: food[v].items[i] && food[v].items[i].name ? food[v].items[i].name : "",
+      });
     }
     doc.id("table").innerHTML += el + '</tr>';
   }
@@ -31,7 +46,7 @@ function setTable() {
 
 function search() {
   var text = doc.id("search").value;
-  if (text.length > 0) {
+  if (text.replaceAll(" ", "").length > 0) {
     doc.id("link").style.display = "block";
     doc.id("link").href = "https://www.google.com/search?q=can%20dog%20eat%20{0}".format(text.replaceAll(" ", "%20"));
     doc.id("link_text").innerHTML = text.capWords();
@@ -43,78 +58,133 @@ function search() {
     doc.id("output").innerHTML = "";
     return;
   }
+  text = text.lower();
 
-  text = text.lower().split(" ");
   results = {};
-  for (r = 0; r < food.length; r++) {
-    for (j = 0; j < food[r].items.length; j++) {
-      let res = food[r].items[j].lower();
-      for (w = 0; w < res.split(" ").length; w++) {
-        for (n = 0; n < text.length; n++) {
-          if (
-            text[n].length > 0
-            && res.split(" ")[w].strip(F.chars.lower).includes(text[n])
-          ) {
-            results.add("{0}_{1}".format(r, j), 1, 1);
+  for (v = 0; v < food.length; v++) {
+    for (i = 0; i < food[v].items.length; i++) {
+      key = "{0}_{1}".format(v, i);
+      num = 0;
+      for (l = 0; l < food[v].items[i].name.split(" ").length; l++) {
+        for (j = 0; j < text.split(" ").length; j++) {
+          if (!text.split(" ")[j]) {
+            continue;
+          }
+          if (food[v].items[i].name.split(" ")[l].includes(text.split(" ")[j])) {
+            num += 10;
           }
         }
       }
+      for (t = 0; t < food[v].items[i].tags.length; t++) {
+        if (!food[v].items[i].tags[t]) {
+          continue;
+        }
+        for (l = 0; l < food[v].items[i].tags[t].split(" ").length; l++) {
+          for (j = 0; j < text.split(" ").length; j++) {
+            if (!text.split(" ")[j]) {
+              continue;
+            }
+            if (food[v].items[i].tags[t].split(" ")[l].includes(text.split(" ")[j])) {
+              num += 1;
+            }
+          }
+        }
+        if (text.includes(food[v].items[i].tags[t])) {
+          num += 7;
+        }
+      }
+      if (num) {
+        if (!results[key]) {
+          results[key] = {
+            name: food[v].items[i].name,
+            subtitle: food[v].items[i].subtitle,
+            group: food[v].name,
+            num: 0,
+            v,
+            i,
+          };
+        }
+        results[key].num = num;
+      }
     }
   }
-
-  temp = [];
-  for (i = 0; i < results.keys().length; i++) {
-    temp.push({
-      title: food[results.keys()[i].split("_")[0]].items[results.keys()[i].split("_")[1]],
-      search: results.values()[i],
-      group: food[results.keys()[i].split("_")[0]].name,
-    });
-  }
-  let length = temp.length;
-  results = F.toArray(temp.sub(0, 10));
-  results.sort(function (a, b) {
-    return (b.search - a.search);
+  results.sort((a, b) => {
+    return (a.num > b.num);
   });
-  delete temp;
 
   el = '<tr>';
   done = [];
-  for (v = 0; v < results.length; v++) {
-    if (!done.includes(results[v].group)) {
+  for (v = 0; v < results.keys().length; v++) {
+    if (!done.includes(results.values()[v].group)) {
       el += '<th>{0}</th>'.format(
-        results[v].group,
+        results.values()[v].group,
       );
-      done.push(results[v].group);
+      done.push(results.values()[v].group);
     }
   }
   doc.id("output").innerHTML = el + '</tr>';
 
-  groups = {};
-  for (i = 0; i < results.length; i++) {
-    if (!groups[done.indexOf(results[i].group)]) {
-      groups[done.indexOf(results[i].group)] = [];
+  groups = [];
+  for (i = 0; i < results.keys().length; i++) {
+    if (!groups[done.indexOf(results.values()[i].group)]) {
+      groups[done.indexOf(results.values()[i].group)] = [];
     }
-    groups[done.indexOf(results[i].group)].push(results[i].title);
-
-    /* console.log("{group} {title}".format({
-      search: results[i].search,
-      title: results[i].title,
-      group: (results[i].group + ":").fill(20, " ", false),
-    })); */
+    groups[done.indexOf(results.values()[i].group)].push(results.values()[i]);
   }
 
   length = 0;
-  for (i = 0; i < groups.keys().length; i++) {
-    length = Math.max(length, groups.values()[i].length);
+  for (i = 0; i < groups.length; i++) {
+    length = Math.max(length, groups[i].length);
   }
 
   for (i = 0; i < length; i++) {
     el = '<tr>';
-    for (v = 0; v < groups.keys().length; v++) {
-      el += '<td>{0}</td>'.format(
-        groups.values()[v][i] ? groups.values()[v][i] : "",
-      );
+    for (v = 0; v < groups.length; v++) {
+      el += [
+        '<td title="{title}" id="{id}">',
+        '  <button {onclick} class="tableButton {hover}">',
+        '   {name}',
+        '   <br>',
+        '   <span class="subtitle">',
+        '     {subtitle}',
+        '   </span>',
+        '  </button>',
+        '</td>'
+      ].join("").format({
+        id: groups[v][i] && "search_" + groups[v][i].v + "_" + groups[v][i].i,
+        onclick: groups[v][i] && 'onclick="clickItem(this);"',
+        hover: groups[v][i] ? "hover" : "",
+        title: groups[v][i] && groups[v][i].tags && groups[v][i].tags.length > 0 && groups[v][i].tags[0] ? "Search Terms: " + groups[v][i].tags.join(", ").capWords() : "",
+        subtitle: groups[v][i] && groups[v][i].subtitle ? groups[v][i].subtitle : "",
+        name: groups[v][i] ? groups[v][i].name : "", 
+      });
     }
     doc.id("output").innerHTML += el + '</tr>';
   }
+}
+
+function clickItem(el) {
+  el = el.parentNode;
+  item = el.id.split("_");
+  item = food[item[1]].items[item[2]];
+  el = [
+    '<h1>',
+    '  {name}',
+    '</h1>',
+    '<h2>',
+    '  {subtitle}',
+    '</h2>',
+    '<p>',
+    '  {desc}',
+    '</p>',
+    '<a href="https://www.google.com/search?q=can dog eat {link}">',
+    '  Search google for \'{name}\'',
+    '</a>',
+  ].join("").format({
+    name: item.name,
+    link: item.name.lower(),
+    subtitle: item.subtitle ? item.subtitle : "",
+    desc: item.desc ? item.desc : "No information",
+  });
+  doc.id("desc").innerHTML = el;
 }
