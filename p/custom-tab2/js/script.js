e04845a72fcd3752f6fbe54bfa47b71ee042aa4f
DarccMan <darcy3057@gmail.com>
2021-02-14 09:26:47 +1100

    CustomTab2

--- p/custom-tab2/js/script.js
+++ p/custom-tab2/js/script.js
@@ -2,27 +2,39 @@
 function init() {
   ls.check();
   sc.init();
+  header.init();
+  doc.id("content").style.visibility = "visible";
+  doc.id("fallback").style.display = "none";
 }
+
 
 /* LocalStorage */
 var ls = {};
+
 ls.check = function () {
+  return;
   if (!ls.get(true)) {
     ls.reset();
   }
 }
+
 ls.reset = function () {
-  ls.set(() => ({
-    sc: [],
-  }));
+  ls.set(() => ({}));
   sc.reset();
+  header.reset();
 }
+
 ls.get = function (dontCheck) {
   if (!dontCheck) {
     ls.check();
   }
-  return (JSON.parse(F.ls("all")));
+  l = JSON.parse(F.ls("all"));
+  if (F.isJSON(l)) {
+    l = JSON.parse(l);
+  }
+  return (l);
 }
+
 ls.set = function (fnct) {
   d = ls.get(true);
   ret = fnct(d);
@@ -34,25 +46,44 @@ ls.set = function (fnct) {
   return (false);
 }
 
+ls.export = function () {
+  prompt("Press CTRL+C to copy", F.ls("all"));
+}
+ls.import = function (str) {
+  if (str) {
+    if (typeof str == "string") {
+      ls.set(() => str);
+    } else {
+      ls.set(() => JSON.stringify(str));
+    }
+  } else {
+    text = prompt("Paste in the settings\nONLY DO THIS IF YOU KNOW WHAT YOU ARE DOING", "Example: '{\"sc\":{},\"header\":\"New-tab\"}'");
+    if (text && F.isJSON(text)) {
+      ls.set(() => text);
+      init();
+    } else {
+      alert("That is not valid JSON");
+    }
+  }
+}
+
+
 /* Shortcuts */
-sc = {};
+var sc = {};
 
 sc.reset = function () {
-  arr = [];
-  for (i = 0; i < data.shortcut_amount; i++) {
-    arr.push({
-      href: null,
-      name: null,
-    });
-  }
   ls.set(d => {
-    d.sc = arr;
+    d.sc = {};
   });
+  // init();
 }
 
 sc.init = function () {
   doc.id("sc_contain").innerHTML = "";
   d = ls.get();
+  if (!d.sc) {
+    sc.reset();
+  }
   for (i = 0; i < data.shortcut_amount; i++) {
     href = "https://epicwebsite.github.io/";
     unknown = "unknown";
@@ -60,7 +91,7 @@ sc.init = function () {
       href = d.sc[i].href;
       unknown = "";
     }
-    name = "Shortcut";
+    name = "Shortcut {0}".format(i + 1);
     if (d.sc[i] && d.sc[i].name) {
       name = d.sc[i].name;
     }
@@ -68,7 +99,7 @@ sc.init = function () {
     doc.id("sc_contain").innerHTML += [
       '<article class="sc {unknown}" id="sc_n{num};">',
       '  <button class="sc link">',
-      '    <a href="{href}" title="Go to \'{link}\'" class="{unknown}">',
+      '    <a href="{href}" title="Go to \'{href}\'" class="{unknown}">',
       '      {name}',
       '    </a>',
       '  </button>',
@@ -88,7 +119,6 @@ sc.init = function () {
 sc.edit = function (el) {
   unknown = el.parentNode.className.includes("unknown");
   num = parseInt(el.parentNode.id.split("n").s(-1));
-  console.log(num);
   old_href = "https://epicwebsite.github.io/"
   if (
     !unknown
@@ -119,11 +149,41 @@ sc.edit = function (el) {
     }
   } else {
     ls.set(d => {
-      d.sc[num] = {
-        href: null,
-        name: null,
-      };
+      delete d.sc[num];
     });
     sc.init();
   }
+}
+
+
+/* Header */
+var header = {};
+
+header.reset = function () {
+  ls.set(d => {
+    d.header = null;
+  });
+}
+
+header.init = function () {
+  text = ls.get().header ? ls.get().header : "New Tab";
+  doc.id("header").innerText = text;
+  doc.id("title").innerText = text;
+}
+
+header.change = function () {
+  old = ls.get().header;
+  if (!old) {
+    header.reset();
+    old = "New Tab";
+  }
+  text = prompt("Change Header", old);
+  if (text) {
+    ls.set(d => {
+      d.header = text;
+    });
+  } else {
+    header.reset();
+  }
+  header.init();
 }
