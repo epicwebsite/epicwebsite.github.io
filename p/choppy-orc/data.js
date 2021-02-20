cf3b1b0cb11c1eb209ec7b51d86e77fe31b9ffbc
DarccMan <darcy3057@gmail.com>
2021-02-20 20:19:47 +1100

    Solitaire: Critical fix; Pages: Added choppy orc page

---
+++ p/choppy-orc/data.js
@@ -0,0 +1,36 @@
+var canvas = doc.create("canvas");
+canvas.id = "canvas";
+canvas.width = 512;
+canvas.height = 512;
+canvas.setAttribute("oncontextmenu", "return(false);");
+doc.id("canvas_contain").appendChild(canvas);
+var ctx = canvas.getContext("2d");
+
+var gameState = "start";
+
+function reset() {
+
+  gameState = "play";
+}
+
+function render() {
+  ctx.fillCanvas();
+
+}
+
+function main() {
+  update((Date.now() - then) / 1000);
+  render();
+  then = Date.now();
+  requestAnimationFrame(main);
+}
+function update(mod) {
+  /* var keysDown = F.getKeyCodes(controls); */
+  if (gameState == "play") {
+
+  }
+}
+
+var then = Date.now();
+reset();
+main();
