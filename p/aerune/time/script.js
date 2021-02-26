a8135c3fdb5fd8748d39f1e7457d2befb0da5346
DarccMan <darcy3057@gmail.com>
2021-02-27 09:26:36 +1100

    Aerune: Added decimal time, Changed size

--- p/aerune/time/script.js
+++ p/aerune/time/script.js
@@ -1,53 +1,71 @@
 change();
 
 function change() {
-  h = parseInt(doc.id("hour").value) || 0;
-  m = parseInt(doc.id("minute").value) || 0;
-  s = parseInt(doc.id("second").value) || 0;
-  l = 0;
+  hn = parseInt(doc.id("hour").value) || 0;
+  mn = parseInt(doc.id("minute").value) || 0;
+  sn = parseInt(doc.id("second").value) || 0;
+  ln = 0;
 
-  if (h > 23) {
-    h = 24 - h;
+  if (hn > 23) {
+    hn = 24 - hn;
   }
-  if (m > 59) {
-    m = 60 - m;
+  if (mn > 59) {
+    mn = 60 - mn;
   }
-  if (s > 59) {
-    s = 60 - s;
-  }
-
-  if (h < 0) {
-    h = 22 - h;
-  }
-  if (m < 0) {
-    m = 58 - m;
-  }
-  if (s < 0) {
-    s = 58 - s;
+  if (sn > 59) {
+    sn = 60 - sn;
   }
 
-  doc.id("hour").value = h;
-  doc.id("minute").value = m;
-  doc.id("second").value = s;
+  if (hn < 0) {
+    hn = 22 - hn;
+  }
+  if (mn < 0) {
+    mn = 58 - mn;
+  }
+  if (sn < 0) {
+    sn = 58 - sn;
+  }
+
+  doc.id("hour").value = hn;
+  doc.id("minute").value = mn;
+  doc.id("second").value = sn;
 
 
-  h *= (16 / 24);
+  h = hn * (16 / 24);
   ho = h - Math.floor(h);
   h -= ho;
 
-  m += ho;
-  m *= (60 / 64);
+  m = mn + ho;
+  m *= (64 / 60);
   mo = m - Math.floor(m);
   m -= mo;
 
-  s += mo * 60;
-  s *= (60 / 64);
+  s = sn + mo * 60;
+  s *= (64 / 60);
   so = s - Math.floor(s);
   s -= so;
 
   l = so * 1000;
 
-  doc.id("output").value = "{0} : {1} : {2} . {3}".format(h, m, s, Math.round(l / 10));
+  doc.id("output_h").value = "{0} : {1} : {2} . {3}".format(h, m, s, Math.round(l / 10));
+
+  h = hn * (10 / 24);
+  ho = h - Math.floor(h);
+  h -= ho;
+
+  m = mn + ho;
+  m *= (100 / 60);
+  mo = m - Math.floor(m);
+  m -= mo;
+
+  s = sn + mo * 60;
+  s *= (100 / 60);
+  so = s - Math.floor(s);
+  s -= so;
+
+  l = so * 1000;
+
+  doc.id("output_d").value = "{0} : {1} : {2} . {3}".format(h, m, s, Math.round(l / 10));
 }
 
 live_change();
@@ -64,6 +82,15 @@ function live_update() {
 }
 function live_change() {
   if (doc.id("live").checked) {
+    els = doc.class("input");
+    for (i = 0; i < els.length; i++) {
+      els[i].disabled = true;
+    }
     live_update();
+  } else {
+    els = doc.class("input");
+    for (i = 0; i < els.length; i++) {
+      els[i].disabled = false;
+    }
   }
 }
